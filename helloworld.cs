5abb96197df0bfed35b7b8d46e45420390b4edfa
Charles Aylward <L00131535@student.lyit.ie>
2018-04-29 13:31:00 +0100

    Added Hello world c# app

---
+++ helloworld.cs
@@ -0,0 +1,17 @@
+// A Hello World Example! program in C# for LYIT by L00131535
+using System;
+namespace HelloWorld
+{
+    class Hello 
+    {
+        static void Main() 
+        {
+			// Console will display Hello LYIT World
+            Console.WriteLine("Hello LYIT World, its L00131535!");
+
+            // Keep the console window open in debug mode.
+            Console.WriteLine("Press any key to exit.");
+            Console.ReadKey();
+        }
+    }
+}
