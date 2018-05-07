3b482bab57603bdebc163885340d9d7e8cd8076c
Charles Aylward <L00131535@student.lyit.ie>
2018-05-07 13:48:50 +0100

    Made changes to the file to be uploaded to Github Branch

--- helloworld.cs
+++ helloworld.cs
@@ -1,4 +1,4 @@
-// A Hello World Example! program in C# for LYIT by L00131535
+// A Hello World Example! program in C# for LYIT by L00131535, version 2
 using System;
 namespace HelloWorld
 {
@@ -9,7 +9,7 @@ namespace HelloWorld
 			// Console will display Hello LYIT World
             Console.WriteLine("Hello LYIT World, its L00131535!");
 
-            // Keep the console window open in debug mode.
+            // Making changes to the file to demonstrate a new version on Github
             Console.WriteLine("Press any key to exit.");
             Console.ReadKey();
         }
