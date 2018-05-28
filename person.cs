80088fc17e48dc59ea3cd563c38ef7f3be4b4509
fisherjr <fisherjr1975@gmail.com>
2018-05-28 14:57:05 +0100

    Added readme
    addded a test readme file

---
+++ person.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Personal
+{
+    public class Person
+    {
+        public string PersonName { get; set; }
+        public string PersonNumber { get; set; }
+        public string Interest { get; set; }
+    }
+}
