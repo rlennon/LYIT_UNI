a7bc9a4b88d8994fad00095773e4a39e03e46317
Bharathi G <bharathi.lyit@gmail.com>
2018-12-11 00:22:53 +0530

    Add : html and java initial files

---
+++ L00144819HelloWorld.java
@@ -0,0 +1,6 @@
+public class L00144819HelloWorld {
+	public static void main(String[] args) {
+		// Prints "Hello, LYIT from L0012345" to the terminal window.
+		System.out.println("Hello, LYIT from L0012345");
+	}
+}
