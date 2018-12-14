73ef63be0f7b795657d46d9d2de1b4879da01b5c
Bharathi03 <45235542+Bharathi03@users.noreply.github.com>
2018-12-14 16:39:35 +0530

    Update L00144819HelloWorld.java
    modiff : the comments with new student id

--- L00144819HelloWorld.java
+++ L00144819HelloWorld.java
@@ -1,6 +1,6 @@
 public class L00144819HelloWorld {
 	public static void main(String[] args) {
-		// Prints "Hello, LYIT from L0012345" to the terminal window.
-		System.out.println("Hello, LYIT from L0012345");
+		// Prints "Hello, LYIT from L00144819" to the terminal window.
+		System.out.println("Hello, LYIT from L00144819");
 	}
 }
